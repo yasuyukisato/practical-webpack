bde35c3fcf467793b7281de8007aa6f007cc3049
yasuyuki sato <godasper@gmail.com>
2022-01-02 16:12:42 +0900

    エントリーポイントの作成

---
+++ practical-webpack/source/js/app.js
@@ -0,0 +1,12 @@
+// エントリーポイントの作成
+
+import $ from "jquery";
+import velocity from "velocity-animate";
+import { add } from "./modules/math";
+
+console.log("app");
+
+const result = add(2, 4);
+
+$("body").append(result);
+velocity($("h1"), "fadeIn", { duration: 2000, loop: true });
