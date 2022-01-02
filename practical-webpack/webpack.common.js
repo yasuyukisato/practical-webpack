31ebcd7c6ad0fabbd54a8eb072a14f3f078b5cd9
yasuyuki sato <godasper@gmail.com>
2022-01-03 04:49:01 +0900

    splitCunkPluginで最適化したファイルを出力する

--- practical-webpack/webpack.common.js
+++ practical-webpack/webpack.common.js
@@ -17,6 +17,17 @@ module.exports = {
     // 複数のファイルを出力するため[name]をつけてplaceholderとして使用
     // entryポイントの名前であるappとanotherが[name]に入る
     filename: "[name].bundle.js",
+    // エントリーポイント以外から出力するファイル名を指定できる
+    chunkFilename: "js/[name].js",
+  },
+  optimization: {
+    splitChunks: {
+      // 分割の対象を記入 initialを指定すると静的にimportしているモジュールが対象
+      // デフォルトでnode_modules以下のモジュールが対象のためjqueryとvelocityが指定される
+      chunks: "initial",
+      // 分割して出力するファイルの名前
+      name: "vendor",
+    },
   },
   // プラグインを指定する設定
   plugins: [
