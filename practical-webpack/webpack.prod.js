aa10f8ad54edd781140171019fbc40f82459ee78
yasuyuki sato <godasper@gmail.com>
2022-01-02 18:03:25 +0900

    console.logを排除して本番ファイルを出力させる

--- practical-webpack/webpack.prod.js
+++ practical-webpack/webpack.prod.js
@@ -1,8 +1,22 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
 
 // 開発用の設定;
 module.exports = merge(commonConfig, {
   mode: "production",
-  watch: true,
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        // ライブラリー等のライセンスコメントの記述を防ぐ
+        extractComments: false,
+        terserOptions: {
+          compress: {
+            //console.logを排除して出力
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
 });
